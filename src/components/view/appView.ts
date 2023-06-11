43566ad18d83f59b44d2e50e84e1dd33c2264f78
Dimash <dinmukhamed.amirov@gmail.com>
2023-06-11 15:28:21 +0600

    feat: add ts

---
+++ src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import { ISource, INews } from '../../types/index';
+
+export class AppView {
+  private news: News;
+  private sources: Sources;
+
+  constructor() {
+    this.news = new News();
+    this.sources = new Sources();
+  }
+
+  public drawNews(data: INews): void {
+    const values = data?.articles ? data?.articles : [];
+    this.news.draw(values);
+  }
+
+  public drawSources(data: ISource) {
+    const values = data?.sources ? data?.sources : [];
+    this.sources.draw(values);
+  }
+}
+
+export default AppView;
