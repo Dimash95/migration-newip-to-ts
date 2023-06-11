e4ab980580ebf49a3c6ad5bb5420a9f6c3ff6f9c
Dimash <dinmukhamed.amirov@gmail.com>
2023-06-11 15:26:48 +0600

    feat: add INews, IArticles

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,28 +1,34 @@
-export interface ISourсe {
-    status: string;
-    sources: ISourceItem[];
+export interface ISource {
+  status: string;
+  sources: ISourceItem[];
 }
 
 export interface ISourceItem {
-    id: string;
-    name: string;
-    description: string;
-    url: string;
-    category: string;
-    language: string;
-    country: string;
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
 }
 
 export interface INews {
-    source: {
-        id: string | null;
-        name: string;
-    };
-    author: string;
-    title: string;
-    description: string;
-    url: string;
-    urlToImage: string;
-    publishedAt: string;
-    content: string;
+  articles: IArticles[];
+  status: string;
+  totalResults: number;
+}
+
+export interface IArticles {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
 }
